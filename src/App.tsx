8cf0bb6ab2c6648e7b1048294fc7eef7ba86484f
dailyco <dailyco03@gmail.com>
2024-09-10 01:39:25 +0900

    ✨feat: ThemeProvider 구현 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
+import { ThemeProvider } from "@/contexts/theme-provider";
+import { STORAGE_KEYS } from "./constants/storage-keys";
 
 type AppProps = {};
 
 const App: React.FC<AppProps> = () => {
-  return <>hello, world</>;
+  return (
+    <ThemeProvider storageKey={STORAGE_KEYS.THEME}>hello, world</ThemeProvider>
+  );
 };
 
 export default App;
